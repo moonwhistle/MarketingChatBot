79c9ba741bb396f285aa0c0787053b20defb0133
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-03-26 14:36:47 +0900

    feat: build project

---
+++ src/main/java/com/example/marketingChatBot/MarketingChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketingChatBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingChatBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marketingChatBot/MarketingChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketingChatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
