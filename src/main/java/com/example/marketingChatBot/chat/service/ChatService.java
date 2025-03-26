21f531e6905045a3a3653d9992fa96c7e18a2849
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-03-27 02:38:06 +0900

    feat: 질문에 대한 답변 받아오는 기능 구현

--- src/main/java/com/example/marketingChatBot/chat/service/ChatService.java
+++ src/main/java/com/example/marketingChatBot/chat/service/ChatService.java
@@ -2,6 +2,7 @@ package com.example.marketingChatBot.chat.service;
 
 import com.example.marketingChatBot.chat.controller.dto.request.ChatRequest;
 
+import com.example.marketingChatBot.chat.service.client.ChatClient;
 import java.util.List;
 
 import org.springframework.ai.document.Document;
@@ -16,13 +17,22 @@ public class ChatService {
 
     private final EmbeddingModel embeddingModel;
     private final VectorStore vectorStore;
+    private final ChatClient chatClient;
 
-    public ChatService(EmbeddingModel embeddingModel, @Qualifier("vectorDB") VectorStore vectorStore) {
+    public ChatService(EmbeddingModel embeddingModel, @Qualifier("vectorDB") VectorStore vectorStore,
+                       ChatClient chatClient) {
         this.embeddingModel = embeddingModel;
         this.vectorStore = vectorStore;
+        this.chatClient = chatClient;
     }
 
-    public String searchRelatedData(ChatRequest request) {
+    public String getAnswer(ChatRequest request) {
+        String relatedData = searchRelatedData(request);
+        String answer = chatClient.callOpenAiApi(request.message(), relatedData);
+        return answer;
+    }
+
+    private String searchRelatedData(ChatRequest request) {
         List<Document> relatedData =  vectorStore.similaritySearch(
                 SearchRequest.builder()
                         .query(request.message())
